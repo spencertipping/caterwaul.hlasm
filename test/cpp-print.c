7895ae5ee318d760e666a8472eae1751daee8db4
Spencer Tipping <spencer@spencertipping.com>
2011-01-20 09:23:14 -0700

    Rebuilt cpp-print again. Looks like we need a stdcall interface rather than the current separate-call-stack approach.

--- test/cpp-print.c
+++ test/cpp-print.c
@@ -22,72 +22,72 @@ exit:
 return *d;
 
 // Begin g1: [[. [.[a]z E tsj] [,w, Kn:i] /]]
-g1:
+g1:fprintf(stderr, "\n%""s %""d %""s", "g1", &&g1 - (long long)&main, "[[. [.[a]z E tsj] [,w, Kn:i] /]]");
 /* [ */ *--d = &&g2;
-/* ] */ goto **c++;
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End g1
 
 // Begin g2: [. [.[a]z E tsj] [,w, Kn:i] /]
-g2:
-/* . */ *--d = d[1];
-/*   */ ;
+g2:fprintf(stderr, "\n%""s %""d %""s", "g2", &&g2 - (long long)&main, "[. [.[a]z E tsj] [,w, Kn:i] /]");
+/* . */ fprintf(stderr, "\n%""4s", "."); *--d = d[1]; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 /* [ */ *--d = &&g3;
-/*   */ ;
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 /* [ */ *--d = &&g6;
-/*   */ ;
-/* / */ goto **((d += 3) - 2 - !d[-1]);
-/* ] */ goto **c++;
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* / */ fprintf(stderr, "\n%""4s", "/"); goto **((d += 3) - 2 - !d[-1]); fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End g2
 
 // Begin g3: [.[a]z E tsj]
-g3:
-/* . */ *--d = d[1];
+g3:fprintf(stderr, "\n%""s %""d %""s", "g3", &&g3 - (long long)&main, "[.[a]z E tsj]");
+/* . */ fprintf(stderr, "\n%""4s", "."); *--d = d[1]; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 /* [ */ *--d = &&g4;
-/* z */ tmp = *d++; *D++=*d++; *--c = &&g5; goto *tmp; g5: *--d = *--D;
-/*   */ ;
-/* E */ --*d;
-/*   */ ;
-/* t */ *--d = 2;
-/* s */ *d = d[*d + 1];
-/* j */ goto **d++;
-/* ] */ goto **c++;
+/* z */ fprintf(stderr, "\n%""4s", "z"); tmp = *d++; *D++=*d++; *--c = &&g5; goto *tmp; g5: *--d = *--D; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* E */ fprintf(stderr, "\n%""4s", "E"); --*d; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* t */ fprintf(stderr, "\n%""4s", "t"); *--d = 2; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* s */ fprintf(stderr, "\n%""4s", "s"); *d = d[*d + 1]; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* j */ fprintf(stderr, "\n%""4s", "j"); goto **d++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End g3
 
 // Begin g4: [a]
-g4:
-/* a */ d[1] += *d++;
-/* ] */ goto **c++;
+g4:fprintf(stderr, "\n%""s %""d %""s", "g4", &&g4 - (long long)&main, "[a]");
+/* a */ fprintf(stderr, "\n%""4s", "a"); d[1] += *d++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End g4
 
 // Begin g6: [,w, Kn:i]
-g6:
-/* , */ ++d;
-/* w */ tmp = *d; *d = d[1]; d[1] = tmp;
-/* , */ ++d;
-/*   */ ;
-/* K */ *--d = 1;
-/* n */ *d = -*d;
-/* : */ *d = gs + *d;
-/* i */ *--c = &&g7; goto **d++; g7:;
-/* ] */ goto **c++;
+g6:fprintf(stderr, "\n%""s %""d %""s", "g6", &&g6 - (long long)&main, "[,w, Kn:i]");
+/* , */ fprintf(stderr, "\n%""4s", ","); ++d; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* w */ fprintf(stderr, "\n%""4s", "w"); tmp = *d; *d = d[1]; d[1] = tmp; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* , */ fprintf(stderr, "\n%""4s", ","); ++d; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* K */ fprintf(stderr, "\n%""4s", "K"); *--d = 1; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* n */ fprintf(stderr, "\n%""4s", "n"); *d = -*d; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* : */ fprintf(stderr, "\n%""4s", ":"); *d = gs + *d; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* i */ fprintf(stderr, "\n%""4s", "i"); *--c = &&g7; goto **d++; g7:; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End g6
 
 // Begin main: k10 kw [[. [.[a]z E tsj] [,w, Kn:i] /]]Z tsj
-main:
-/* k */ *--d = 0;
-/* 1 */ *d *= 10; *d += 1;
-/* 0 */ *d *= 10; *d += 0;
-/*   */ ;
-/* k */ *--d = 0;
-/* w */ tmp = *d; *d = d[1]; d[1] = tmp;
-/*   */ ;
+main:fprintf(stderr, "\n%""s %""d %""s", "main", &&main - (long long)&main, "k10 kw [[. [.[a]z E tsj] [,w, Kn:i] /]]Z tsj");
+/* k */ fprintf(stderr, "\n%""4s", "k"); *--d = 0; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* 1 */ fprintf(stderr, "\n%""4s", "1"); *d *= 10; *d += 1; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* 0 */ fprintf(stderr, "\n%""4s", "0"); *d *= 10; *d += 0; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* k */ fprintf(stderr, "\n%""4s", "k"); *--d = 0; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* w */ fprintf(stderr, "\n%""4s", "w"); tmp = *d; *d = d[1]; d[1] = tmp; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 /* [ */ *--d = &&g1;
-/* Z */ tmp = *d++; *D++=*d++; *D++=*d++; *--c = &&g8; goto *tmp; g8: *--d=*--D; *--d=*--D;
-/*   */ ;
-/* t */ *--d = 2;
-/* s */ *d = d[*d + 1];
-/* j */ goto **d++;
-/* ] */ goto **c++;
+/* Z */ fprintf(stderr, "\n%""4s", "Z"); tmp = *d++; *D++=*d++; *D++=*d++; *--c = &&g8; goto *tmp; g8: *--d=*--D; *--d=*--D; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/*   */ fprintf(stderr, "\n%""4s", " "); ; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* t */ fprintf(stderr, "\n%""4s", "t"); *--d = 2; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* s */ fprintf(stderr, "\n%""4s", "s"); *d = d[*d + 1]; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* j */ fprintf(stderr, "\n%""4s", "j"); goto **d++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
+/* ] */ fprintf(stderr, "\n%""4s", "]"); goto **c++; fprintf(stderr, "%""10d|s%""10d|c%""20lld|v%""20lld|dv%""20lf|V", data + DATA_S - d, code + CODE_S - c, *d, *d - (long long)&main, *d);
 // End main
 
 }
